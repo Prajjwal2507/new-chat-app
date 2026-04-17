90113623a9da59739e79d6f81f134ed6c158c1ad
Prajjwal2507 <prajjwalrajput2507@gmail.com>
2026-04-18 01:59:27 +0530

    loderpage removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,8 +15,6 @@ function App() {
   }, [checkAuth]);
 
 
-  if (isCheckingAuth) return <PageLoader />;
-
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center sm:p-4 overflow-x-hidden">
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
